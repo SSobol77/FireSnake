9061c2a15890d6591035fd32915eadb1c003e77b
Siergej Sobolewski <108773983+SSobol77@users.noreply.github.com>
2022-11-11 00:55:53 +0300

    v 1.1

---
+++ src/main/java/GameField.java
@@ -0,0 +1,76 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+
+/**
+ *  здесь вся логика
+ */
+
+public class GameField extends JPanel {
+    private final int SIZE = 640; // константа размерность нашего поля игры
+    private final int DOT_SIZE = 16; // константа размерность нашей точки
+    private final int ALL_DOTS = 800 ; // максимальное количество звеньев цепочки элементов
+
+    // проинициализируем картинки:
+    private Image dot;
+    private Image apple;
+
+    //объявляем два массива наших координат X и Y с размерностью массива ALL_DOTS,
+    // т.к. 800 точек, то будет 800 Х-координат:
+    private int[] x = new int[ALL_DOTS];
+    private int[] y = new int[ALL_DOTS];
+
+    // координаты X и Y яблока:
+    private int appleX;
+    private int appleY;
+
+    // Колличество очков т.е. количество звеньев змейки:
+    private int dots;
+
+    // Timer - класс, который считает время, он будет считать количество кадров в секунду,
+    // т.е. чем больше кадров у нас обновляется каждую секунду, тем быстрее движется наша змейка:
+    private Timer timer;
+
+    // задаем булевскую переменную, когда будет равна false, то мы выходим из программы:
+    private boolean inGame = true;
+
+    // создадим метод загружающий картинки в переменные dot и apple:
+    private void loadImage(){
+
+        ImageIcon iid = new ImageIcon("dot.png");
+        dot = iid.getImage();
+
+        ImageIcon iia = new ImageIcon("apple.png");
+        apple = iia.getImage();
+    }
+
+    // создадим метод генерирующий случайные числа, чтобы рандомно генерировать координаты appleX и appleY:
+    private void createApple(){
+        Random random = new Random();
+        appleX = random.nextInt(40) * DOT_SIZE;
+        appleY = random.nextInt(40) * DOT_SIZE;
+    }
+
+    // создадим метод инициализирующий начало игры
+    public void initGame(){
+        dots = 3;
+        for (int i = 0; i < 5; i++) {
+            y[i] = 48; // начало по y
+            x[i] = 48 - i * DOT_SIZE; // начало по x пять
+        }
+
+        // проинициализируем таймер чем больше число тем медленнее двигается змейка
+        timer = new Timer(150,);
+        timer.start(); // запускаем таймер на выполнение чтобы он все обнулял
+        createApple(); // создаем яблоко
+    }
+
+    // создадим метод проверяющий не съела ли змейка яблоко
+    public void checkApple(){
+        if(x[0] == appleX && y[0] == appleY){ // если координаты змейки совпадают с яблоком, то:
+            dots++; // количество точек змейки увеличиваем на единицу
+            createApple(); // вызываем метод, который придумывает новые рандомные координаты и создаем новое яблоко
+        }
+    }
+
+}
--- src/main/java/MainWindow.java
+++ src/main/java/MainWindow.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class MainWindow {
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Начинаем работу над нашим первым проектом. Не будем ограничиваться консолью, поэтому нам понадобиться окно,
+ * в котором и будут происходить все действия. Будем использовать swing.
+ */
+public class MainWindow extends JFrame {
+    public MainWindow(){ // создаем конструктор нашего класса
+
+        setTitle("Fire Snake"); // Титул названия окна
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);    //когда нажмем красный крестик программа завершит работу,
+                                                                    // если не прописать? то программа зависала бы в памяти компа
+        setSize(640,665);  // размер поля окна в пикселях этот размер 320х320+25pix для заголовка окна "Fire Snake"
+        setLocation(400, 400); //координаты появления змейки относительно верхнего левого угла
+        setVisible(true);    // окно появляется при запуске
+    }
+    //..точка входа в программу :
+    public static void main(String[] args) {
+        MainWindow mainWindow = new MainWindow(); // создаем обьект mainWindow, по сути просто вызываем созданный ранее конструктор
+    }
 }
